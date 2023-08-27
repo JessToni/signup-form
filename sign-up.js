1975636951bd80227a1a29a70b2e2696c61bc1c0
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-28 04:29:57 +0800

    Change span text when passwords do not match

--- sign-up.js
+++ sign-up.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     let password = document.querySelector("#password").value;
     let confirmPassword = document.querySelector("#confirm").value;
+
+    let error = document.querySelector(".error");
+
+    if ((password == "" && confirmPassword == "") || password != confirmPassword) {
+        error.innerHTML = "* Passswords do not match";
+    } else if (password == confirmPassword) {
+        error.innerHTML = "";
+    }
 })
