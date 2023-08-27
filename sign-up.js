8443923570d44ff5a0361afdf2a1f5b200673fe0
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-28 04:10:24 +0800

    Make change id of confirm password to "confirm"

--- sign-up.js
+++ sign-up.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirm").value;
 })
